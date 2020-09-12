c5a2ae5a6403df04943296db703f442975d14ace
bharatvarmagit <itisbharatvarma@gmail.com>
2020-09-12 09:45:49 -0400

    prints reciept with custom inputs

--- main.js
+++ main.js
@@ -1,55 +1,58 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 exports.__esModule = true;
 var prompt = require('prompt-sync')({ sigint: true });
-var getItems = function () {
-    var items;
-};
-var getItem = function () { return __awaiter(void 0, void 0, void 0, function () {
-    var name, quantity, isImported;
-    return __generator(this, function (_a) {
-        name = getName();
-        quantity = getQuantity();
-        isImported = getIsImported();
-        console.log("is impoerted boolean : ", isImported);
-        return [2 /*return*/];
+var Item_1 = require("./Item");
+var IMPORTTAX = 0.05;
+var TAX = 0.1;
+var printReciept = function (mapObject, totalPriceBeforeTax, totalSalesTax) {
+    var totalPriceAfterTax = totalPriceBeforeTax + totalSalesTax;
+    mapObject.forEach(function (value, key) {
+        var multQuantity = value.totalQuantity > 1 ? " (" + value.totalQuantity + " @ " + value.unitPrice : '';
+        console.log(key + ' : ' + value.totalQuantity * value.unitPrice + multQuantity);
     });
-}); };
+    console.log('Sales Tax : ' + totalSalesTax);
+    console.log('Total : ' + totalPriceAfterTax);
+};
+var countTotalQuantityPerItem = function (items) {
+    // have to count quantity for items with same name and isImported
+    var totalPriceBeforeTax = 0;
+    var totalSalesTax = 0;
+    var quantCountMap = new Map();
+    items.forEach(function (item) {
+        totalPriceBeforeTax += item.quantity * item.unitPrice;
+        totalSalesTax += item.quantity * item.unitPrice * TAX;
+        if (item.isImported) { //item is imported
+            totalSalesTax += item.quantity * item.unitPrice * IMPORTTAX;
+            if (quantCountMap.has('Imported ' + item.name))
+                quantCountMap.get('Imported ' + item.name).totalQuantity += item.quantity;
+            else
+                quantCountMap.set('Imported ' + item.name, { totalQuantity: item.quantity, unitPrice: item.unitPrice });
+        }
+        else { //item is not imported
+            if (quantCountMap.has(item.name))
+                quantCountMap.get(item.name).totalQuantity += item.quantity;
+            else
+                quantCountMap.set(item.name, { totalQuantity: item.quantity, unitPrice: item.unitPrice });
+        }
+    });
+    printReciept(quantCountMap, totalPriceBeforeTax, Number((Math.round(totalSalesTax * 20) / 20).toFixed(2)));
+};
+var getItems = function () {
+    var items = new Array();
+    var input = prompt("want to add an item? press y/Y else n/N : ");
+    while (input.match(/y|Y/)) {
+        items.push(getItem());
+        input = prompt("want to add an item? press y/Y else n/N : ");
+    }
+    countTotalQuantityPerItem(items);
+};
+var getItem = function () {
+    var name = getName();
+    var quantity = getQuantity();
+    var isImported = getIsImported();
+    var unitPrice = getUnitPrice();
+    return new Item_1["default"](quantity, isImported, name, unitPrice);
+};
 var getName = function () {
     var promptName = "Enter the name of the Product : ";
     var name;
@@ -67,8 +70,17 @@ var getQuantity = function () {
         quantity = prompt(promptQuantity);
         promptQuantity = "Expected Number, Recieved String! " + promptQuantity;
     } while (isNaN(Number(quantity)));
-    console.log("QTY is " + quantity);
-    return quantity;
+    return Number(quantity);
+};
+var getUnitPrice = function () {
+    var promptUnitPrice = "enter the UnitPrice  : ";
+    var unitPrice;
+    do {
+        unitPrice = prompt(promptUnitPrice);
+        promptUnitPrice = "Expected Number, Recieved String! " + promptUnitPrice;
+    } while (isNaN(Number(unitPrice)));
+    console.log("UnitPrice  is " + unitPrice);
+    return Number(unitPrice);
 };
 var getIsImported = function () {
     var promptIsImported = "is it Imported (Y/N) : ";
@@ -83,6 +95,6 @@ var getIsImported = function () {
     return false;
 };
 function main() {
-    getItem();
+    getItems();
 }
 main();
--- main.ts
+++ main.ts
@@ -1,23 +1,56 @@
-
 const prompt = require('prompt-sync')({sigint: true});
-import { isString } from 'util';
 import Item from './Item';
+const IMPORTTAX:number=0.05;
+const TAX:number=0.1;
 
+const printReciept=(mapObject,totalPriceBeforeTax:number,totalSalesTax:number)=>{
+  const totalPriceAfterTax:number=totalPriceBeforeTax+totalSalesTax;
+  mapObject.forEach((value,key)=>{
+    let multQuantity=value.totalQuantity>1?` (${value.totalQuantity} @ ${value.unitPrice}`:''
+    console.log(key+' : '+value.totalQuantity*value.unitPrice+multQuantity);
+  })
+  console.log('Sales Tax : '+totalSalesTax);
+  console.log('Total : '+totalPriceAfterTax);
+}
 
+const countTotalQuantityPerItem=(items:Item[])=>{
+  // have to count quantity for items with same name and isImported
+  let totalPriceBeforeTax=0;
+  let totalSalesTax=0;
+  let quantCountMap=new Map();
+  items.forEach((item:Item)=>{
+    totalPriceBeforeTax+=item.quantity*item.unitPrice;
+    totalSalesTax+=item.quantity*item.unitPrice*TAX;
+    if (item.isImported){ //item is imported
+      totalSalesTax+=item.quantity*item.unitPrice*IMPORTTAX;
+      if (quantCountMap.has('Imported '+item.name))  quantCountMap.get('Imported ' + item.name).totalQuantity += item.quantity;
+      else quantCountMap.set('Imported '+item.name,{totalQuantity:item.quantity,unitPrice:item.unitPrice});
+    }
+    else{ //item is not imported
+      if(quantCountMap.has(item.name)) quantCountMap.get(item.name).totalQuantity+=item.quantity;
+      else quantCountMap.set(item.name,{totalQuantity:item.quantity,unitPrice:item.unitPrice});
+    }
+  });
+  printReciept(quantCountMap,totalPriceBeforeTax,Number((Math.round(totalSalesTax*20)/20).toFixed(2)));
+}
 const getItems=()=>{
-  let items:Item[];
+  let items:Item[]=new Array();
+  let input = prompt("want to add an item? press y/Y else n/N : ");
+  while(input.match(/y|Y/)){
+    items.push(getItem());
+    input = prompt("want to add an item? press y/Y else n/N : ");
+  }
+  countTotalQuantityPerItem(items);
 }
 
-const getItem=async()=>{
-  let name=getName();
-  let quantity=getQuantity();
-  let isImported=getIsImported();
-  console.log("is impoerted boolean : ",isImported);
-
-  // let isImported=prompt("is it imported? (y/n) : ");
-  // let unitPrice=prompt(`unit price of ${name} : `);
+const getItem =():Item=>{
+  const name=getName();
+  const quantity=getQuantity();
+  const isImported=getIsImported();
+  const unitPrice=getUnitPrice();
+  return new Item(quantity,isImported,name,unitPrice);
 }
-const getName=()=>{
+const getName=():string=>{
   let promptName="Enter the name of the Product : "
   let name ;
   do{
@@ -29,7 +62,7 @@ const getName=()=>{
   return name;
 }
 
-const getQuantity=()=>{
+const getQuantity=():number=>{
   let promptQuantity = "enter the Quantity : "
   let quantity;
   do {
@@ -37,15 +70,23 @@ const getQuantity=()=>{
     promptQuantity= "Expected Number, Recieved String! " + promptQuantity;
   }
   while (isNaN(Number(quantity)));
-  console.log("QTY is " + quantity);
-  return quantity;
+  return Number(quantity);
 }
 
+const getUnitPrice = ():number => {
+  let promptUnitPrice = "enter the UnitPrice  : "
+  let unitPrice ;
+  do {
+    unitPrice  = prompt(promptUnitPrice);
+    promptUnitPrice = "Expected Number, Recieved String! " + promptUnitPrice ;
+  }
+  while (isNaN(Number(unitPrice )));
+  console.log("UnitPrice  is " + unitPrice );
+  return Number(unitPrice );
+}
 
-
-
-const getIsImported = () => {
-  let promptIsImported = "is it Imported (Y/N) : "
+const getIsImported = ():boolean => {
+  let promptIsImported:string = "is it Imported (Y/N) : "
   let isImported;
   do {
     isImported = prompt(promptIsImported);
@@ -59,7 +100,7 @@ const getIsImported = () => {
 
 
 function main(){
-  getItem();
+  getItems();
 }
 
 main();
