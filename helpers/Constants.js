465d5a72de4aac1e543091ef31a0992c3dda73ea
Namburi <56741541+bharatvarmagit@users.noreply.github.com>
2020-09-15 02:29:02 -0400

    Delete Constants.js

--- helpers/Constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.MainPrompt = exports.TAX = exports.IMPORTTAX = void 0;
-exports.IMPORTTAX = 0.05;
-exports.TAX = 0.1;
-exports.MainPrompt = "press 1 to read data from file and 2 to manualy enter data : ";
